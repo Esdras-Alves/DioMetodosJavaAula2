52ed859d7b681c250ba036cb617a42888c219726
Esdras <esdrasbalves10@gmail.com>
2023-01-22 18:25:25 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(5);
+        Quadrilatero.area(5d,7d);
+        Quadrilatero.area(5,7,9);
+        Quadrilatero.area(5f,7f);
+    }
+}
